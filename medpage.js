d8f6dcc2967994304baebb24ac5b1e059d18fbde
raj8888 <jadhavrj8877@gmail.com>
2022-11-10 22:47:41 +0530

    completed day-3

---
+++ medpage.js
@@ -0,0 +1,77 @@
+let data=[]
+fetch("./JSON_Files/med.json")
+.then((res)=>res.json())
+.then((allmed)=>{
+    data=[...allmed]
+ displayammmed(allmed)
+})
+
+
+function search(){
+    let ip=document.querySelector("#itemsearch").value;
+    let newdata=data.filter(function(elem){
+      let ans=elem.type.toLowerCase().includes(ip.toLowerCase())||elem.item.toLowerCase().includes(ip.toLowerCase())||elem.company.toLowerCase().includes(ip.toLowerCase())||elem.disease.toLowerCase().includes(ip.toLowerCase())
+      return ans;
+    })
+    displayammmed(newdata)
+  }
+
+  function typeofmed(){
+    let selected=document.querySelector(".fildata").value;
+    let filterdata=data.filter(function(el){
+        if(selected=="Filter All"){
+            return data;
+        }else{
+            return el.type==selected;
+        }
+    })
+    displayammmed(filterdata)
+  }
+
+  function typeofdis(){
+    let selected=document.querySelector(".fildatadis").value;
+    let filterdata=data.filter(function(el){
+        if(selected=="Filter All"){
+            return data;
+        }else{
+            return el.disease==selected;
+        }
+    })
+    displayammmed(filterdata)
+  }
+
+// let trecat=document.querySelector(".showtrecat")
+function displayammmed(allmed){
+    document.querySelector(".midsec").innerHTML=""
+    allmed.forEach(function (elem,index)  {
+    let div=document.createElement("div")
+    div.setAttribute("class","medbox")
+    let img=document.createElement("img")
+    img.setAttribute("src",elem.img)
+    img.setAttribute("width", "80px")
+    let divinfo=document.createElement("div")
+    divinfo.setAttribute("class","medinfo")
+    let name=document.createElement("h3")
+    name.innerText=elem.item
+    let desc=document.createElement("p")
+    desc.innerText=elem.pre
+    let main=document.createElement("p")
+    main.innerText=elem.company
+    let type=document.createElement("p")
+    type.innerText=elem.type
+    type.style.color="green"
+    let price=document.createElement("p")
+    price.innerText=elem.mprice
+    price.style.textDecoration="line-through"
+    let dis=document.createElement("p")
+    dis.innerText=elem.discount
+    let offprice=document.createElement("p")
+    offprice.innerText=elem.Price
+    let but=document.createElement("button")
+    but.innerText="ADD TO CART"
+    divinfo.append(name,desc,main,price,dis,offprice,type,but)
+    div.append(img,divinfo)
+    document.querySelector(".midsec").append(div)
+ });
+}
+
